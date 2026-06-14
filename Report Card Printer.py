d7ee6975799b98463745694447c1ca36520fa645
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-14 19:30:38 +0530

    Update Report Card Printer.py

--- Report Card Printer.py
+++ Report Card Printer.py
@@ -2,9 +2,9 @@ name = 'Shivam'
 print(name,type(name))
 is_student = True;
 print(name, type(name))
-
 is_student = True
 print(is_student, type(is_student))
-
 age = 20
 print(age, type(age))
+score = 80.5
+print(isinstance(score, int))

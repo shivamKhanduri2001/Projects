44275f238b6f39f1adc00fa8ae1ace3adb90f4a2
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-14 21:53:59 +0530

    Update Movie Ticket Booking Calculator.py

--- Movie Ticket Booking Calculator.py
+++ Movie Ticket Booking Calculator.py
@@ -19,7 +19,7 @@ else :
 print('Discount:', discount)
 extra_charges = 0
 
-if is_weekend:
+if is_weekend or show_time == 'Evening'::
     extra_charges = 2
     print('Extra charges will be applied')
 else:

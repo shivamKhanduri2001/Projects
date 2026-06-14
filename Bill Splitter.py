907cda16b2e11eeb243988b2006de72ad3f97ed4
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-14 20:41:10 +0530

    Update Bill Splitter.py

--- Bill Splitter.py
+++ Bill Splitter.py
@@ -14,3 +14,5 @@ running_total += tip
 print('Total with tip:',running_total)
 final_bill = running_total / num_of_friends
 print('Bill per person:',final_bill)
+each_pays = round(final_bill, 2)
+print('Each person pays:',each_pays)

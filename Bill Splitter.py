9ef734bd27778f02593f8e2d566cc933243dd8dd
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-14 20:35:56 +0530

    Update Bill Splitter.py

--- Bill Splitter.py
+++ Bill Splitter.py
@@ -6,5 +6,9 @@ desserts = 39.39;
 drinks = 64.21;
 running_total += appetizers + main_courses + desserts + drinks;
 print('Total bill so far:', running_total)
+
 tip = running_total * 0.25
 print('Tip amount:', tip)
+
+running_total += tip
+print('Total with tip:',running_total)

c962b06d28652bd2bcda96052705c22fc32355d0
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-14 20:27:43 +0530

    Update Bill Splitter.py

--- Bill Splitter.py
+++ Bill Splitter.py
@@ -4,3 +4,5 @@ appetizers = 37.89;
 main_courses = 57.34;
 desserts = 39.39;
 drinks = 64.21;
+running_total += appetizers + main_courses + desserts + drinks;
+print('Total bill so far:', running_total)

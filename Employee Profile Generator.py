2bca6a38543ca60e7e64b4c9a9233005b923b8fc
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-14 19:43:18 +0530

    Update Employee Profile Generator.py

--- Employee Profile Generator.py
+++ Employee Profile Generator.py
@@ -1,9 +1,9 @@
 first_name = "Shivam";
 last_name = "Khanduri";
-print(first_name);
-print(last_name);
+#print(first_name);
+#print(last_name);
 full_name = first_name+" "+ last_name;
-print(full_name);
+#print(full_name);
 address = '123 Main Street'  
 address += ', Apartment 4B'
-print(address) 
+#print(address) 

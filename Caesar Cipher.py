cf8ba4e6dd7ae6a14924e1a5582c19dc6b772a6b
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-15 07:26:44 +0530

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,6 +1,8 @@
 def caesar():
     if not isinstance(shift,int):
         return 'Shift must be an integer value.'
+    if shift < 1 :
+        return 'Shift must be a positive integer.'  
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())

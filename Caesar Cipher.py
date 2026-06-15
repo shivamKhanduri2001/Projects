7feb149b3e264682403b47f2be2f2746dceabb43
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-15 06:44:19 +0530

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -2,3 +2,4 @@ shift = 5
 alphabet ='abcdefghijklmnopqrstuvwxyz'
 shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
 translation_table = str.maketrans(alphabet, shifted_alphabet)
+text = 'hello world'

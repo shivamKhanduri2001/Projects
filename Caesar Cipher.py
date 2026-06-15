bf3651a16a5f960ec06dde4b76a3feffbf00cabe
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-15 07:38:52 +0530

    Fix error message for invalid shift value

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -2,7 +2,9 @@ def caesar(text, shift, encrypt=True):
     if not isinstance(shift,int):
         return 'Shift must be an integer value.'
     if shift < 1 or shift > 25:
-        return 'Shift must be an integer between 1 and 25.'  
+        return 'Shift must be an integer between 1 and 25.'
+    if not encrypt:
+        shift = -shift
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())

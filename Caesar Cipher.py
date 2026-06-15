d17daa4815a014a2deab9fc3179d2acd72ff18ec
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-15 06:47:53 +0530

    Add encryption functionality to Caesar Cipher

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -3,3 +3,5 @@ alphabet ='abcdefghijklmnopqrstuvwxyz'
 shifted_alphabet = alphabet[shift:] + alphabet[0:shift]
 translation_table = str.maketrans(alphabet, shifted_alphabet)
 text = 'hello world'
+encrypted_text = text.translate(translation_table)
+print(encrypted_text)
